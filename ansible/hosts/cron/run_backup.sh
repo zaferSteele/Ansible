277230a0525914f23a314512ca0e8e4171e2c9cf
Zafer Steele <zafersteele@gmail.com>
2025-06-16 10:21:17 +0100

    Delete ansible directory

--- ansible/hosts/cron/run_backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i /etc/ansible/hosts/inventory.ini --vault-password-file /etc/ansible/hosts/cron/sh_password.txt /etc/ansible/hosts/cron/backup_running_cfg_scp.yml >> /home/user/cronsecure_log.txt 2>&1
